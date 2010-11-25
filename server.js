92fe418ee19f4ae7d33bc3ca993791669acb8a00
Sean Porter <portertech@gmail.com>
2010-11-25 03:40:02 -0800

    severity and facility lookup

--- server.js
+++ server.js
@@ -7,6 +7,44 @@ websocket.listen(8000)
 
 var syslog_regex = /<([0-9]{1,3})>([A-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) (\S+) (.*)/
 
+var severity_lookup = {
+	0: 'emerg',
+	1: 'alert',
+	2: 'crit',
+	3: 'err',
+	4: 'warn',
+	5: 'notice',
+	6: 'info',
+	7: 'debug',
+}
+
+var facility_lookup = {
+	0: 'kern',
+	1: 'user',
+	2: 'mail',
+	3: 'daemon',
+	4: 'auth',
+	5: 'syslog',
+	6: 'lpr',
+	7: 'news',
+	8: 'uucp',
+	9: 'cron',
+	10: 'authpriv',
+	11: 'ftp',
+	12: 'ntp',
+	13: 'audit',
+	14: 'alert',
+	15: 'at',
+	16: 'local0',
+	17: 'local1',
+	18: 'local2',
+	19: 'local3',
+	20: 'local4',
+	21: 'local5',
+	22: 'local6',
+	23: 'local7',
+}
+
 syslog = dgram.createSocket('udp4')
 syslog.on('message', function(msg_orig, rinfo) {
 	var msg = syslog_regex.exec(msg_orig)
@@ -15,11 +53,10 @@ syslog.on('message', function(msg_orig, rinfo) {
 		var msg_info = {
 			date: msg[2],
 			host: msg[3],
-			severity: msg[1] - (facility * 8),
-			facility: facility,
+			severity: severity_lookup[msg[1] - (facility * 8)],
+			facility: facility_lookup[facility],
 			message: msg[4],
 		}
-		console.log(msg_info)
 		websocket.broadcast(JSON.stringify(msg_info))
 	}
 })
