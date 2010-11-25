0be825a3eea6dcbacd9bb530ade51be1c8cf834e
Justin Kolberg <justinkolberg@aelatis.com>
2010-11-25 02:51:18 -0800

    Weeee.... WebSockets!!

---
+++ public/scripts/websocket.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+//<![CDATA[
+var conn;
+var connect = function() {
+  if (window["WebSocket"]) {
+    conn = new WebSocket("ws://localhost:8000");
+	conn.onopen = function(){
+		//alert('Connected to the WebSocket!!'); // Uncomment this if you're in doubt that your browser supports WebSockets
+	};
+	conn.onmessage = function(evt){
+		data = JSON.parse(evt.data);
+		$('#messagesTable > tbody:last').append('<tr><td>'+
+		data['date']+'</td><td>'+
+		data['host']+'</td><td>'+
+		data['severity']+'</td><td>'+
+		data['facility']+'</td><td>'+
+		data['message']+'</td></tr>');
+	};
+  }
+};
+
+window.onload = connect;
+//]]>
+</script>
