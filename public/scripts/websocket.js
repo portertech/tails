af9ce5a1d096b636f60e0ec174dbf3926edec4e9
Justin Kolberg <justinkolberg@aelatis.com>
2010-12-03 09:51:18 -0800

    Added table row trimming after a default of 50 messages. Modified styles.

--- public/scripts/websocket.js
+++ public/scripts/websocket.js
@@ -1,11 +1,12 @@
 var conn;
+var maxNumRows = 50;
 var connect = function() {
   if (window["WebSocket"]) {
     conn = new WebSocket("ws://portertech.no.de:8000");
-	conn.onopen = function(){
+	conn.onopen = function() {
 		//alert('Connected to the WebSocket!!'); // Uncomment this if you're in doubt that your browser supports WebSockets
 	};
-	conn.onmessage = function(evt){
+	conn.onmessage = function(evt) {
 		data = JSON.parse(evt.data);
 		$('#messagesTable > tbody:last').prepend('<tr class="row"><td>'+
 		data['date']+'</td><td>'+
@@ -14,8 +15,13 @@ var connect = function() {
 		data['facility']+'</td><td class="message"><div class="hideextra">'+
 		data['message']+'</div></td></tr>');
 		$('tr.row:first').click(function() {
+			$(this).toggleClass('message_expanded');
 			$(this).children('td.message').children('div').toggleClass('hideextra');
 		});
+		var num_rows = $('#messagesTable > tbody > tr').size();
+		if (num_rows > maxNumRows) {
+			$('#messagesTable > tbody:last > tr:last').remove();
+		}
 	};
   }
 };
