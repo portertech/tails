3d78818c5a53927307ec9619bc9de90e31733a79
Sean Porter <portertech@gmail.com>
2010-12-04 13:26:30 -0800

    config syslog test port

--- test/syslog_test.js
+++ test/syslog_test.js
@@ -1,13 +1,14 @@
 var dgram = require('dgram')
 
-var client = dgram.createSocket("udp4")
+var syslog = dgram.createSocket("udp4")
 
 var message = new Buffer("<71>Nov 23 17:22:23 pirate_ship all the piracy. none of the scurvy! all ye be walkin the plank if me timbers be shiverin. polly does not want a cracker but i will taketh one from ye.")
 
-client.send(message, 0, message.length, 5140, "127.0.0.1",
+syslog_port = parseInt(process.ENV.TAILS_SYSLOG_PORT) || 514
+syslog.send(message, 0, message.length, syslog_port, '127.0.0.1',
 	function (err, bytes) {
 		if (err) {
 			throw err
 		}
-	console.log("Wrote " + bytes + " bytes to socket.")
+	console.log('Wrote ' + bytes + ' bytes to socket.')
 })
