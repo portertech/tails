407909ad598eb7dfdffb5bdc142b3de3f2a3ec9d
Justin Kolberg <justinkolberg@aelatis.com>
2010-12-06 15:56:03 -0800

    Divided logs into tabs and sexified it.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-	$('div#alertstext').toggle(
-		function() {
-			$('div#alertscontainer').slideDown('slow', function() {
-				$(this).removeClass('container_hidden');
-			});
-		},
-		function() {
-			$('div#alertscontainer').slideUp('slow', function() {
-				$(this).addClass('container_hidden');
+	$('div#alerts_text').click(function() {
+		if (!$(this).hasClass('tab_selected')) {
+			$('div#all_text').removeClass('tab_selected', 'normal');
+			$('div#all').fadeOut('normal', function() {
+				$('div#alerts_text').addClass('tab_selected', 'normal');
+				$('div#alerts').fadeIn('normal');
 			});
 		}
-	);
+	});
+	$('div#all_text').click(function() {
+		if (!$(this).hasClass('tab_selected')) {
+			$('div#alerts_text').removeClass('tab_selected', 'normal');
+			$('div#alerts').fadeOut('normal', function() {
+				$('div#all_text').addClass('tab_selected', 'normal');
+				$('div#all').fadeIn('normal');
+			});
+		}
+	});
 });
