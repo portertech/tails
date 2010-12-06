96af1c137d9063ef206f14e4c27e618b5fe2d57b
Justin Kolberg <justinkolberg@aelatis.com>
2010-12-06 04:25:37 -0800

    Added example regex for matching a tag. Alerts count now works.

--- public/scripts/websocket.js
+++ public/scripts/websocket.js
@@ -1,5 +1,6 @@
 var conn;
 var maxNumRows = 200;
+var alertsCount = 0;
 var connect = function() {
   if (window["WebSocket"]) {
     conn = new WebSocket("ws://portertech.no.de:8000");
@@ -8,12 +9,30 @@ var connect = function() {
 	};
 	conn.onmessage = function(evt) {
 		data = JSON.parse(evt.data);
+		
+		if (/ssh/i.test(data['message'])) {
+			$('#alertsnum').html(++alertsCount);
+			
+			$('#alertsTable > tbody:last').prepend('<tr class="row"><td>'+
+			data['date']+'</td><td>'+
+			data['host']+'</td><td>'+
+			data['severity']+'</td><td>'+
+			data['facility']+'</td><td class="message"><div class="hideextra">'+
+			data['message']+'</div></td></tr>');
+			
+			var alerts_num_rows = $('#alertsTable > tbody > tr').size();
+			if (alerts_num_rows > maxNumRows) {
+				$('#alertsTable > tbody:last > tr:last').remove();
+			}
+		}
+		
 		$('#messagesTable > tbody:last').prepend('<tr class="row"><td>'+
 		data['date']+'</td><td>'+
 		data['host']+'</td><td>'+
 		data['severity']+'</td><td>'+
 		data['facility']+'</td><td class="message"><div class="hideextra">'+
 		data['message']+'</div></td></tr>');
+		
 		$('tr.row:first').click(function() {
 			$(this).toggleClass('message_expanded');
 			$(this).children('td.message').children('div').toggleClass('hideextra');
