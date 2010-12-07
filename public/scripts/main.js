bce33f61824e714c5c2f7f3c5d91ce8994b7cd27
Justin Kolberg <justinkolberg@aelatis.com>
2010-12-06 16:50:19 -0800

    Fixed bug where both tabs could be selected.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,19 +1,27 @@
+var inAnimation = false;
+
 $(document).ready(function() {
 	$('div#alerts_text').click(function() {
-		if (!$(this).hasClass('tab_selected')) {
+		if ((!$(this).hasClass('tab_selected')) && inAnimation == false) {
+			inAnimation = true;
 			$('div#all_text').removeClass('tab_selected', 'normal');
 			$('div#all').fadeOut('normal', function() {
 				$('div#alerts_text').addClass('tab_selected', 'normal');
-				$('div#alerts').fadeIn('normal');
+				$('div#alerts').fadeIn('normal', function() {
+					inAnimation = false;
+				});
 			});
 		}
 	});
 	$('div#all_text').click(function() {
-		if (!$(this).hasClass('tab_selected')) {
+		if ((!$(this).hasClass('tab_selected')) && inAnimation == false) {
+			inAnimation = true;
 			$('div#alerts_text').removeClass('tab_selected', 'normal');
 			$('div#alerts').fadeOut('normal', function() {
 				$('div#all_text').addClass('tab_selected', 'normal');
-				$('div#all').fadeIn('normal');
+				$('div#all').fadeIn('normal', function() {
+					inAnimation = false;
+				});
 			});
 		}
 	});
