7f13ec884ef4f0973c3ad28f6236b1aa2a0bfb70
Justin Kolberg <justinkolberg@aelatis.com>
2010-12-15 19:14:50 -0800

    Removed an alert used for testing.

--- public/scripts/websocket.js
+++ public/scripts/websocket.js
@@ -51,7 +51,6 @@ var connect = function() {
 		
 		/* Remove the last line if there are more than maxNumRows */
 		var num_rows = $('div#logs_container > div#all_logs > table > tbody:last > tr').size();
-		alert(num_rows);
 		if (num_rows > maxNumRows) {
 			$('#div#logs_container > div#all_logs > table > tbody:last > tr:last').remove();
 		}
