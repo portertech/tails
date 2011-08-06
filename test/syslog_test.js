b87b3e322e50d2a12efed94dd8b7a8894f29840f
Sean Porter <portertech@gmail.com>
2011-08-06 11:08:17 -0700

    get demo working on joyent no.de smartmachine

--- test/syslog_test.js
+++ test/syslog_test.js
@@ -11,8 +11,9 @@ var options = require("nomnom").opts({
 var message = new Buffer("<71>Nov 23 17:22:23 pirate_ship-127-0-0-1 all the piracy. none of the scurvy! all ye be walkin the plank if me timbers be shiverin. polly does not want a cracker but i will taketh one from ye.")
 
 var syslog = dgram.createSocket("udp4")
+var syslogPort = process.env.TAILS_SYSLOG_PORT || options.syslog
 setInterval(function() {
-  syslog.send(message, 0, message.length, options.syslog, '127.0.0.1',
+  syslog.send(message, 0, message.length, syslogPort, '127.0.0.1',
     function (err, bytes) {
       if (err) {
         throw err
